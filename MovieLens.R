b76c7629d5ced3884f850fb79085686f0de775c2
arpitagupta16 <arpitagupta16@gmail.com>
2019-06-17 00:34:17 +0800

    Add files via upload

---
+++ MovieLens.R
@@ -0,0 +1,92 @@
+library(tidyverse)
+library(caret)
+library(data.table)
+library(kableExtra)
+library(lubridate)
+library(DT)
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+
+# MovieLens 10M dataset:
+# https://grouplens.org/datasets/movielens/10m/
+# http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+dl <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings <- read.table(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+                      col.names = c("userId", "movieId", "rating", "timestamp"))
+
+movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+colnames(movies) <- c("movieId", "title", "genres")
+movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))[movieId],
+                                           title = as.character(title),
+                                           genres = as.character(genres))
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# Validation set will be 10% of MovieLens data
+
+set.seed(1) # if using R 3.6.0: set.seed(1, sample.kind = "Rounding")
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+# Make sure userId and movieId in validation set are also in edx set
+
+validation <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from validation set back into edx set
+
+removed <- anti_join(temp, validation)
+edx <- rbind(edx, removed)
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+
+#i define the RMSE function as:
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+# regularization 
+
+# lambda is a tuning parameter. We can use cross-validation to choose it
+
+lambdas <- seq(0, 10, 0.25)
+
+rmses <- sapply(lambdas, function(l){
+  
+  mu_reg <- mean(edx$rating)
+  
+  b_i_reg <- edx %>% 
+    group_by(movieId) %>%
+    summarize(b_i_reg = sum(rating - mu_reg)/(n()+l))
+  
+  b_u_reg <- edx %>% 
+    left_join(b_i_reg, by="movieId") %>%
+    group_by(userId) %>%
+    summarize(b_u_reg = sum(rating - b_i_reg - mu_reg)/(n()+l))
+  
+  predicted_ratings_b_i_u <- 
+    validation %>% 
+    left_join(b_i_reg, by = "movieId") %>%
+    left_join(b_u_reg, by = "userId") %>%
+    mutate(pred = mu_reg + b_i_reg + b_u_reg) %>%
+    .$pred
+  
+  return(RMSE(validation$rating,predicted_ratings_b_i_u))
+})
+
+
+qplot(lambdas, rmses)  
+
+#For the full model, the optimal  ?? is:
+
+lambda <- lambdas[which.min(rmses)]
+lambda
+
+rmse_model <- min(rmses)
+rmse_model
